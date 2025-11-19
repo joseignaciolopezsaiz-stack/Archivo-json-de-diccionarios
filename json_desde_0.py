a94b6d206e5cf69cf73780ec913419e5d00852b7
Jose Ignacio Lopez <jose.ignacio.lopez.saiz@gmail.com>
2025-11-19 16:34:47 +0100

    Add files via upload

---
+++ json_desde_0.py
@@ -0,0 +1,41 @@
+import json
+
+file_name = "users_data.json"
+data = {}
+with open(file_name, "w") as f:
+    json.dump(data,f, indent=4)
+    
+def usuario():   
+    file_name = "users_data.json"
+    #cargar archivo json completo
+    
+    with open(file_name,"r") as f:
+        data = json.load(f)
+    print(data)
+    if len(data) == 0:
+        a = 0
+        #print(a)
+        a = str(a+1)
+        b = 'emp'+a
+        #print(b)
+    #print(len(data))
+    if len(data) > 0:
+        a =  len(data)   
+        #print(a)
+        a = str(a +1)
+        b = 'emp'+a
+    #print(b)
+# Agregar una nueva entrada
+    nombre = input("nombre: ")
+    edad = input("edad: ")
+    salario = input("salario: ")
+    data[b] = {'nombre': nombre, 'edad': edad,"salario":salario}
+    with open(file_name, "w") as f:
+        json.dump(data,f, indent=4)
+    with open(file_name,"r") as f:
+        print(json.load(f))
+usuario()
+usuario()
+usuario()
+usuario()
+
